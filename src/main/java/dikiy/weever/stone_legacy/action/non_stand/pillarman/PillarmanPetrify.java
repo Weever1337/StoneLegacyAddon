31b1f3b5a4625af5bbf0f540cd2ca648541fd0c6
Weever <weever1337@gmail.com>
2025-05-16 18:36:34 +0300

    Tried to do Petrify, but fucking crash NullPointerException... ``` [18:31:08] [Server thread/ERROR] [ne.mi.ev.EventBus/EVENTBUS]: Exception caught during firing event: null 	Index: 1 	Listeners: 		0: NORMAL 		1: ASM: class com.github.standobyte.jojo.util.GameplayEventHandler onPlayerTick(Lnet/minecraftforge/event/TickEvent$PlayerTickEvent;)V 		2: ASM: com.github.standobyte.jojo.client.ControllerStand@79e1f733 sendPlayerPositionRotation(Lnet/minecraftforge/event/TickEvent$PlayerTickEvent;)V java.lang.NullPointerException 	at com.github.standobyte.jojo.power.impl.PowerBaseImpl.tick(PowerBaseImpl.java:114) 	at com.github.standobyte.jojo.power.impl.nonstand.NonStandPower.tick(NonStandPower.java:117) 	at com.github.standobyte.jojo.util.GameplayEventHandler.lambda$onPlayerTick$3(GameplayEventHandler.java:276) 	at net.minecraftforge.common.util.LazyOptional.ifPresent(LazyOptional.java:165) 	at com.github.standobyte.jojo.util.GameplayEventHandler.onPlayerTick(GameplayEventHandler.java:275) 	at net.minecraftforge.eventbus.ASMEventHandler_69_GameplayEventHandler_onPlayerTick_PlayerTickEvent.invoke(.dynamic) 	at net.minecraftforge.eventbus.ASMEventHandler.invoke(ASMEventHandler.java:85) 	at net.minecraftforge.eventbus.EventBus.post(EventBus.java:302) 	at net.minecraftforge.eventbus.EventBus.post(EventBus.java:283) 	at net.minecraftforge.fml.hooks.BasicEventHooks.onPlayerPreTick(BasicEventHooks.java:85) 	at net.minecraft.entity.player.PlayerEntity.tick(PlayerEntity.java:196) 	at net.minecraft.entity.player.ServerPlayerEntity.doTick(ServerPlayerEntity.java:404) 	at net.minecraft.network.play.ServerPlayNetHandler.tick(ServerPlayNetHandler.java:207) 	at net.minecraft.network.NetworkManager.tick(NetworkManager.java:226) 	at net.minecraft.network.NetworkSystem.tick(NetworkSystem.java:134) 	at net.minecraft.server.MinecraftServer.tickChildren(MinecraftServer.java:865) 	at net.minecraft.server.MinecraftServer.tickServer(MinecraftServer.java:787) 	at net.minecraft.server.integrated.IntegratedServer.tickServer(IntegratedServer.java:78) 	at net.minecraft.server.MinecraftServer.runServer(MinecraftServer.java:642) 	at net.minecraft.server.MinecraftServer.lambda$spin$0(MinecraftServer.java:232) 	at java.lang.Thread.run(Thread.java:750) 	```

--- src/main/java/dikiy/weever/stone_legacy/action/non_stand/pillarman/PillarmanPetrify.java
+++ src/main/java/dikiy/weever/stone_legacy/action/non_stand/pillarman/PillarmanPetrify.java
@@ -3,15 +3,12 @@ package dikiy.weever.stone_legacy.action.non_stand.pillarman;
 import com.github.standobyte.jojo.action.ActionConditionResult;
 import com.github.standobyte.jojo.action.ActionTarget;
 import com.github.standobyte.jojo.action.non_stand.PillarmanStoneForm;
+import com.github.standobyte.jojo.power.IPower;
 import com.github.standobyte.jojo.power.impl.nonstand.INonStandPower;
 import dikiy.weever.stone_legacy.util.StoneLegacyUtil;
-import net.minecraft.block.Blocks;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.entity.player.ClientPlayerEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
-import net.minecraft.network.play.client.CClientStatusPacket;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.util.text.ITextComponent;
@@ -20,6 +17,7 @@ import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 public class PillarmanPetrify extends PillarmanStoneForm {
@@ -39,7 +37,19 @@ public class PillarmanPetrify extends PillarmanStoneForm {
 
     @Override
     protected void perform(World world, LivingEntity user, INonStandPower power, ActionTarget target) {
-        ((PlayerEntity) user).respawn(); // why the hell it is not working
+        if (!user.level.isClientSide() && user instanceof ServerPlayerEntity)
+        {
+            ServerPlayerEntity player = (ServerPlayerEntity) user;
+            Optional<Vector3d> pos = PlayerEntity.findRespawnPositionAndUseSpawnBlock((ServerWorld) player.level, Objects.requireNonNull(player.getRespawnPosition()), player.getRespawnAngle(), player.isRespawnForced(), true);
+            if (pos.isPresent()) {
+                float angle = player.getRespawnAngle();
+                player.moveTo(pos.get().x, pos.get().y, pos.get().z, angle, 0);
+//                player.setPlayerInput();
+            } else {
+                return;
+            }
+        }
+        power.clear();
     }
 
     @Override
